5942fe133de4a646b1bc8d36f1806af7d3622ec9
Alexandr Melnic <alexandr.melnic@student.kit.edu>
2022-04-12 19:56:28 +0100

    Multiplexer added

--- imu.py
+++ imu.py
@@ -1,10 +1,22 @@
 import adafruit_bno055
+import adafruit_tca9548a
 import board
 import time
+import busio
 
-i2c = board.I2C()
+# i2c = board.I2C()
+i2c = busio.I2C(board.SCL, board.SDA)
 
-sensor = adafruit_bno055.BNO055_I2C(i2c, address=0x28)
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+for channel in range(8):
+    if tca[channel].try_lock():
+        print("Channel {}:".format(channel), end="")
+        addresses = tca[channel].scan()
+        print([hex(address) for address in addresses if address != 0x70])
+        tca[channel].unlock()
+
+sensor = adafruit_bno055.BNO055_I2C(i2c, int(tca[0]))
 
 while True:
     print(sensor.temperature)
